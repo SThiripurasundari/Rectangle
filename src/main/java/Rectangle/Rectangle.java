a83211de2889e588447865cc8fe100a25e4c9a07
SThiripurasundari <thiripurasundari.selvaraj@gmail.com>
2022-03-10 15:55:47 +0530

    Third Commit

--- src/main/java/Rectangle/Rectangle.java
+++ src/main/java/Rectangle/Rectangle.java
@@ -5,14 +5,14 @@ public class Rectangle {
     private final float length;
     private final float width;
 
+
     public Rectangle(float length, float width) {
         this.length = length;
         this.width = width;
     }
 
     public float area() {
-
-        return length*width;
+      return length*width;
 
     }
 }
