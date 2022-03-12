c8cb0dd5406f5295cccd1ad3e83f1d96201825fc
SThiripurasundari <thiripurasundari.selvaraj@gmail.com>
2022-03-12 15:29:30 +0530

    Refactored and applied Factory method pattern

--- src/main/java/Rectangle/Rectangle.java
+++ src/main/java/Rectangle/Rectangle.java
@@ -5,9 +5,17 @@ public class Rectangle {
     private final float length;
     private final float width;
 
-    public Rectangle(float length, float width) {
+    private Rectangle(float length, float width) {
         this.length = length;
         this.width = width;
+    }
+
+    public static Rectangle square(float side) {
+        return new Rectangle(side, side);
+    }
+
+    public static Rectangle rectangle(float length, float width) {
+        return new Rectangle(length, width);
     }
 
     public float area() {
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -8,7 +8,7 @@ public class RectangleTest {
     @Test
     void checkForArea20ForLength4andWidth5() {
 
-        Rectangle rectangle = new Rectangle(4, 5);
+        Rectangle rectangle = Rectangle.rectangle(4, 5);
 
         float area = rectangle.area();
 
@@ -19,7 +19,7 @@ public class RectangleTest {
     @Test
     void checkForArea16ForLength4AndWidth4() {
 
-        Rectangle rectangle = new Rectangle(4, 4);
+        Rectangle rectangle = Rectangle.rectangle(4, 4);
 
         float area = rectangle.area();
 
@@ -29,7 +29,7 @@ public class RectangleTest {
     @Test
     void checkForAreaWithFloatForLengthAndWidth() {
 
-        Rectangle rectangle = new Rectangle(4.5f, 4.5f);
+        Rectangle rectangle = Rectangle.rectangle(4.5f, 4.5f);
 
         float area = rectangle.area();
 
@@ -39,7 +39,7 @@ public class RectangleTest {
     @Test
     void checkForPerimeter18ForLength4AndWidth5() {
 
-        Rectangle rectangle = new Rectangle(4, 5);
+        Rectangle rectangle = Rectangle.rectangle(4, 5);
 
         float perimeter = rectangle.perimeter();
 
@@ -51,7 +51,7 @@ public class RectangleTest {
     @Test
     void checkForPerimeterFloat12point0ForLength2point0Andwidth4point0() {
 
-        Rectangle rectangle = new Rectangle(2.0f, 4.0f);
+        Rectangle rectangle = Rectangle.rectangle(2.0f, 4.0f);
 
         float perimeter = rectangle.perimeter();
 
@@ -62,11 +62,90 @@ public class RectangleTest {
     @Test
     void checkForPerimeter28ForLength8AndWidth6() {
 
-        Rectangle rectangle = new Rectangle(8, 6);
+        Rectangle rectangle = Rectangle.rectangle(8, 6);
 
         float perimeter = rectangle.perimeter();
 
         assertEquals(28, perimeter);
 
     }
+
+    @Test
+    void checkForArea16ForSide4() {
+
+        Rectangle square = Rectangle.square(4);
+
+        float area = square.area();
+
+        assertEquals(16, area);
+
+    }
+
+    @Test
+    void checkForArea81ForSide9() {
+
+        Rectangle square = Rectangle.square(9);
+
+        float area = square.area();
+
+        assertEquals(81, area);
+
+    }
+
+
+    @Test
+    void checkForArea25point0ForSide5point0() {
+
+        Rectangle square = Rectangle.square(5.0f);
+
+        float area = square.area();
+
+        assertEquals(25, area);
+
+    }
+
+    @Test
+    void checkForArea33point0625For5point75() {
+        Rectangle square = Rectangle.square(5.75f);
+
+        float area = square.area();
+
+        assertEquals(33.0625, area);
+    }
+
+
+    @Test
+    void CheckForPerimeter28ForSide4() {
+
+        Rectangle square = Rectangle.square(7);
+
+        float perimeter = square.perimeter();
+
+        assertEquals(28, perimeter);
+
+    }
+
+    @Test
+    void CheckForPerimeter100ForSide10() {
+
+        Rectangle square = Rectangle.square(10);
+
+        float perimeter = square.perimeter();
+
+        assertEquals(40, perimeter);
+
+    }
+
+    @Test
+    void CheckForPerimeter32point0ForSide8point0() {
+
+        Rectangle square = Rectangle.square(8.0f);
+
+        float perimeter = square.perimeter();
+
+        assertEquals(32.0, perimeter);
+
+    }
+
+
 }
